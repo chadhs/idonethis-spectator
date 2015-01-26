e25a4f8922caeb61911f727bb476342fbd9d276e
Chad Stovern <chadhs@digitalnomad.im>
2015-01-26 10:29:32 -0600

    replaced static test dates with datetime based start and end date variables for calculating last week, jinja template is now rendering unicode characters and links properly. also added a feature to call out incomplete goals

--- spectator.py
+++ spectator.py
@@ -3,12 +3,14 @@
 from flask import Flask
 from flask import render_template
 import requests
+import datetime
 
 
 ## configuration
 token = ""
 team = ""
 user = ""
+idturl= "https://idonethis.com"
 
 
 ## the app
@@ -29,57 +31,34 @@ def hello():
 
 
 ## TODO
-#- date math to replace static dates
-#- proper goal display
+# - load variables from config file
 @app.route("/dones")
 def display_dones():
-    url_today = "https://idonethis.com/api/v0.1/dones/?done_date=today&owner=%s&team=%s" % (user, team)
+    startdate = datetime.date.today() - datetime.timedelta(1)
+    enddate = datetime.date.today() - datetime.timedelta(7)
+
+    url_today = "https://idonethis.com/api/v0.1/dones/?done_date=today&owner=%s&team=%s&page_size=100" % (user, team)
     dones_today = get_json_data(url_today)
     dones_today_text = []
-    for done in dones_today:
-        dones_today_text.append(done['raw_text'])
-        #dones_today_text.append(done['markedup_text'])
+    for d in dones_today:
+        done = d['markedup_text']
+        goal = d['goal_completed']
+        done = done.replace("/hashtags","%s/hashtags" % (idturl), 1)
+        done = done.replace("/cal","%s/cal" % (idturl), 1)
+        dones_today_text.append((done,goal))
 
-    url_lastweek = "https://idonethis.com/api/v0.1/dones/?done_date_after=2015-01-15&done_date_before=2015-01-20&owner=%s&team=%s" % (user, team)
+    url_lastweek = "https://idonethis.com/api/v0.1/dones/?done_date_after=%s&done_date_before=%s&owner=%s&team=%s&page_size=100" % (enddate, startdate, user, team)
     dones_lastweek = get_json_data(url_lastweek)
     dones_lastweek_text = []
-    for done in dones_lastweek:
-        dones_lastweek_text.append(done['raw_text'])
+    for d in dones_lastweek:
+        done = d['markedup_text']
+        goal = d['goal_completed']
+        done = done.replace("/hashtags","%s/hashtags" % (idturl), 1)
+        done = done.replace("/cal","%s/cal" % (idturl), 1)
+        dones_lastweek_text.append((done,goal))
 
     return render_template('dones.html', team=team, user=user, results_today=dones_today_text, results_week=dones_lastweek_text)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-## done data for reference ##
-
-#done_date=`date '+%Y-%m-%d'`
-#two_days_ago=`date -v -2d '+%Y-%m-%d'`
-#eight_days_ago=`date -v -8d '+%Y-%m-%d'`
-
-#{
-    #"comments": [],
-    #"created": "2015-01-14T20:12:15.446",
-    #"done_date": "2015-02-16",
-    #"goal_completed": true,
-    #"id": 17387024,
-    #"is_goal": false,
-    #"likes": [],
-    #"markedup_text": "<a href=\"/hashtags/witc/#tags/vacation\" rel=\"nofollow\">#vacation</a> day off, returning from skiing",
-    #"meta_data": {},
-    #"owner": "smalter",
-    #"permalink": "https://idonethis.com/done/17387024/",
-    #"raw_text": "#vacation day off, returning from skiing",
-    #"tags": [
-        #{
-            #"id": 91336,
-            #"name": "vacation"
-        #}
-    #],
-    #"team": "https://idonethis.com/api/v0.1/teams/witc/",
-    #"team_short_name": "witc",
-    #"updated": "2015-01-14T20:12:15.530",
-    #"url": "https://idonethis.com/api/v0.1/dones/17387024/"
-#}
