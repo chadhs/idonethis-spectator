3c04b58967aa452eab1e5859b4b526b15dfb81d6
Chad Stovern <chadhs@digitalnomad.im>
2015-02-06 08:30:06 -0600

    created another helper function called fix_rel_url to be called more than once so i wouldn't be repeating code manually.  added a couple simple doc strings to functions. also set dones endpoint url as a variable since it was repeated.

--- spectator.py
+++ spectator.py
@@ -11,8 +11,8 @@ import config
 token = config.token
 team = config.team
 user = config.user
-idturl= "https://idonethis.com"
-
+idturl = "https://idonethis.com"
+api_dones_url = idturl + '/api/v0.1/dones/?'
 
 ## the app
 app = Flask(__name__)
@@ -20,10 +20,22 @@ app = Flask(__name__)
 
 ## helpers
 def get_json_data(url):
+    """
+    fetch dones from the iDoneThis api, return list of dones from the json response
+    """
     headers = {'content-type': 'application/json', 'authorization': 'token %s' % (token)}
     r = requests.get(url, headers=headers)
     data = r.json()
     dones = data['results']
+    return dones
+
+def fix_rel_url(dones):
+    """
+    replace relative urls in markedup_text with absolute urls
+    """
+    for done in dones:
+        done['markedup_text'] = done['markedup_text'].replace("/hashtags","%s/hashtags" % (idturl))
+        done['markedup_text'] = done['markedup_text'].replace("/cal","%s/cal" % (idturl))
     return dones
 
 
@@ -33,17 +45,13 @@ def display_dones():
     startdate = datetime.date.today() - datetime.timedelta(1)
     enddate = datetime.date.today() - datetime.timedelta(7)
 
-    url_today = "https://idonethis.com/api/v0.1/dones/?done_date=today&owner=%s&team=%s&page_size=100" % (user, team)
+    url_today = "%sdone_date=today&owner=%s&team=%s&page_size=100" % (api_dones_url, user, team)
     dones_today = get_json_data(url_today)
-    for done in dones_today:
-        done['markedup_text'] = done['markedup_text'].replace("/hashtags","%s/hashtags" % (idturl))
-        done['markedup_text'] = done['markedup_text'].replace("/cal","%s/cal" % (idturl))
+    dones_today = fix_rel_url(dones_today)
 
-    url_lastweek = "https://idonethis.com/api/v0.1/dones/?done_date_after=%s&done_date_before=%s&owner=%s&team=%s&page_size=100" % (enddate, startdate, user, team)
+    url_lastweek = "%sdone_date_after=%s&done_date_before=%s&owner=%s&team=%s&page_size=100" % (api_dones_url, enddate, startdate, user, team)
     dones_lastweek = get_json_data(url_lastweek)
-    for done in dones_lastweek:
-        done['markedup_text'] = done['markedup_text'].replace("/hashtags","%s/hashtags" % (idturl))
-        done['markedup_text'] = done['markedup_text'].replace("/cal","%s/cal" % (idturl))
+    dones_lastweek = fix_rel_url(dones_lastweek)
 
     return render_template('dones.html', team=team, user=user, dones_today=dones_today, dones_lastweek=dones_lastweek)
 
